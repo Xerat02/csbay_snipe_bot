02de7e2a9a3f98411f2f2bb49f964f6467b18527
dienstbiera <dienstbiera@spseplzen.cz>
2024-07-16 22:56:16 +0200

    folder structures changes

--- markets_scripts/waxpeer.py
+++ markets_scripts/waxpeer.py
@@ -14,7 +14,7 @@ async def on_add_item(data):
         price = str(round((float(data['price'])/1000),2))
         if "★" in name:
             link_name = link_name[1:]
-        link = "https://waxpeer.com/"+link_name+"/item/"+str(data['item_id'])
+        link = "https://waxpeer.com/r/csbay/"+link_name+"/item/"+str(data['item_id'])
         items.append(f"{name};{price};{link};Waxpeer")
     except Exception as e:
         logging.error("Error occurred during getting data: %s", e)
